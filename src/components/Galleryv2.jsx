85d39a5237a0fd5dbb029a8989917316c29edf55
SmithinJRaj <smithinjraj@gmail.com>
2026-09-11 00:25:19 +0530

    feat: added star bg in tathva gallery

--- src/components/Galleryv2.jsx
+++ src/components/Galleryv2.jsx
@@ -13,7 +13,7 @@ const baseGalleryImages = [
 
 const Gallery = forwardRef((props, ref) => {
   return (
-    <div ref={ref} id="galleryx" className="my-auto mb-14 bg-black">
+    <div ref={ref} id="galleryx" className="my-auto mb-14 bg-transparent relative z-10">
       <div className="flex justify-center items-center px-4 sm:px-8 lg:px-16 sm:py-12">
         <p className="text-center max-w-3xl text-gray-300 plus-jakarta leading-relaxed tracking-wide font-light">
           <span className="bg-gradient-to-r pp-fragment from-white via-gray-300 to-white bg-clip-text text-transparent text-3xl sm:text-5xl block mb-6 sm:mb-10">
