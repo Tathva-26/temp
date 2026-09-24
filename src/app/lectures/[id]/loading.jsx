78fb20daa44468be7eacb73521384cd990423e3f
PR202111 <prashantraj251195@gmail.com.com>
2026-09-24 13:27:37 +0530

    Removed the delay and added zoom effect properly

---
+++ src/app/lectures/[id]/loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className='bg-transparent min-h-screen flex items-center justify-center'>
+      <div className='inline-block h-12 w-12 animate-spin rounded-full border-4 border-solid border-white border-r-transparent' />
+    </div>
+  )
+}
